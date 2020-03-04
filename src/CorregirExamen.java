78bf3305e83f6deaa14877e6ac8fcc6a28f9eff1
idbis <idbis@idbis-SATELLITE-PRO-A50-C>
2020-03-04 16:22:58 +0100

    Merge origin/master
    Conflicts:
    	src/Evaluacion.java

---
+++ src/CorregirExamen.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import entornos.examen.*;
+
+
+public class CorregirExamen {
+
+    /*
+        Debéis crear un objeto de la clase que habéis creado que implemente Corregible, y añadirlo a la lista.
+    
+     */
+    public static void main(String[] args) {
+
+        //DEFINIR LA LISTA
+        ArrayList<Corregible> lista = new ArrayList<Corregible>();
+
+        // ZONA PARA CREAR OBJETOS Y AÑADIRLOS A LA LISTA
+
+        
+        
+        // MOSTRAR EL CONTENIDO DE LA LISTA
+        for (Corregible objeto : lista) {
+            System.out.println(objeto.descripcion());
+        }
+
+    }
+
+}
