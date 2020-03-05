9ec44f3fb7d1ee05fe6fe0cecda4fa133b9423e0
Usuario <Usuario@DESKTOP-68GH1E6>
2020-03-05 18:50:20 +0100

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties

---
+++ src/entornos/examen/AlejandroMB.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos.examen;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AlejandroMB implements Corregible {
+    
+    @Override
+    public String descripcion(){
+        return "Alejandro M. B.";
+    }   
+}
