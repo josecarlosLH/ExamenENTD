c39ac31fef146371de65134c1baeb8d6344a36b5
Jose <Jose.ant.du@gmail.com>
2020-03-05 18:09:37 +0100

    Merge origin/Prueba1
    Conflicts:
    	src/com/mojang/ld22/Game.java

---
+++ src/entornos/examen/JoseAntonioDP.java
@@ -0,0 +1,14 @@
+package entornos.examen;
+
+/**
+ *
+ * @author Jose Antonio Duarte Perez
+ */
+public class JoseAntonioDP implements Corregible{
+
+    @Override
+    public String descripcion() {
+        return "Jose Antonio DP";
+    }
+    
+}
