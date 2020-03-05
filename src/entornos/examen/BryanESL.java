1d97db432b03f35f63330343435c8509b12d9314
PC <PC@DESKTOP-E4AJC4K>
2020-03-05 18:01:41 +0100

    Merge origin/master into HEAD

---
+++ src/entornos/examen/BryanESL.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos.examen;
+
+/**
+ *
+ * @author PC
+ */
+public class BryanESL implements Corregible{
+
+    @Override
+    public String descripcion() {
+        String nombre = "Bryan ESL";  
+        return nombre; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
