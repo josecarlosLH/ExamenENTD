b8962e502dcf4b4683b0bb1a6f918ca17bc1fae6
alexhaake <59737508+alexhaake@users.noreply.github.com>
2020-03-05 18:15:12 +0100

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/entornos/examen/AlexAH.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos.examen;
+
+/**
+ *
+ * @author usuario
+ */
+public class AlexAH implements Corregible{
+
+    @Override
+    public String descripcion() {
+        String resultado;
+        resultado = "ALEX HAAKE A.H ";
+        return resultado;
+    }
+    
+}
