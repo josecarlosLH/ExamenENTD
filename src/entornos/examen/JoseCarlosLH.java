8843544f0b7246aca2318d2a8e65ac55a07b51e7
josecarlosLH <60482743+josecarlosLH@users.noreply.github.com>
2020-03-05 19:37:17 +0100

    Create JoseCarlosLH.java

---
+++ src/entornos/examen/JoseCarlosLH.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornos.examen;
+
+/**
+ *
+ * @author JoseCarls
+ */
+public class JoseCarlosLH implements Corregible{
+
+    @Override
+    public String descripcion() {
+        String nombre = "JoseCarlosLH";  
+        return nombre; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
