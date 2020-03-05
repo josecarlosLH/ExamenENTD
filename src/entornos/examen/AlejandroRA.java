f7569c4d1e7d5aec92c53201ff3fd4003abe3d98
HP <HP@LAPTOP-9VT3DU6B>
2020-03-05 18:42:05 +0100

    Añado nombre

--- src/entornos/examen/AlejandroRA.java
+++ src/entornos/examen/AlejandroRA.java
@@ -1,8 +1,5 @@
 package entornos.examen;
 
-
-import entornos.examen.Corregible;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
